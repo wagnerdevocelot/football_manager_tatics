373c187c38e108c51bfd0607580247df34b96b11
wagnerdevocelot <wagner.dev1337@gmail.com>
2019-08-15 19:24:02 -0300

    usando herança agora posso intanciar jogadores da classe player

---
+++ lib/goal_keeper.rb
@@ -0,0 +1,9 @@
+
+
+require_relative 'player'
+
+
+class Goal < Player
+
+end    
+
--- lib/tatical_app.rb
+++ lib/tatical_app.rb
@@ -1,9 +1,10 @@
 require_relative 'player'
 require_relative 'constants'
+require_relative 'goal_keeper'
 
 
 
-goleiro = Player.new("Manuel Neuer", 32, "Germany", "192cm", "90Kg" )
+goleiro = Goal.new("Manuel Neuer", 32, "Germany", "192cm", "90Kg" )
 
 
 goleiro.technical_stats = {
@@ -23,7 +24,32 @@ goleiro.technical_stats = {
     TECHNIQUE: 15,
 }
 
+goleiro.mental_stats = {
+    AGRESSION: 8,
+    ANTICIPATION: 20,
+    BRAVERY: 14,
+    COMPOSURE: 20,
+    CONCENTRATION: 15,
+    DECISIONS: 16,
+    DETERMINATION: 18,
+    FLAIR: 8,
+    LEADERSHIP: 13,
+    OFF_THE_BALL: 1,
+    POSITIONING: 16,
+    TEAMWORK: 18,
+    VISION: 12,
+    WORK_RATE: 14
+}
 
 
+goleiro.physical_stats = {
+    ACCELERATION: 13,
+    AGILITY: 16,
+    BALANCE: 15,
+    JUMPING_REACH: 15,
+    NATURAL_FITNESS: 13,
+    PACE: 9,
+    STAMINA: 10,
+    STRENGHT: 16
+}
 
-p goleiro
