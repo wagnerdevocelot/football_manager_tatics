657cc3155a8c7f9f1430fc17e7389daceb61f3a1
wagnerdevocelot <wagner.dev1337@gmail.com>
2019-08-19 19:30:20 -0300

    refactoring update

--- lib/central_defense.rb
+++ lib/central_defense.rb
@@ -5,24 +5,19 @@ require_relative 'player'
 class CentralDefender < Player
 
     def description
-        "The main job of the Central Defender is to stop the opposing attackers from playing and to
-        clear the ball from danger when required"
+        "The main job of the Central Defender is to stop the opposing attackers from playing and to clear the ball from danger when required"
     end
 
     def defend
-        "With a Defend duty, the Centre Defender will stay in the line with his defensive partner
-        and look to break up attaks mark opposing forwards and prevent 
-        the ball from getting into the box"
+        "With a Defend duty, the Central Defender will stay in the line with his defensive partner and look to break up attaks mark opposing forwards and prevent the ball from getting into the box."
     end
 
     def stoper
-        "With a Stoper Duty, the Central Defender will push ahead of the defensive line and close 
-        down players before they get to the area"
+        "With a Stoper Duty, the Central Defender will push ahead of the defensive line and close down players before they get to the area"
     end
 
     def cover
-        "With a Covering Duty, the Central Defender will drop a little deeper and sweep 
-        up through balls behind the defensive line"
+        "With a Covering Duty, the Central Defender will drop a little deeper and sweep up through balls behind the defensive line"
     end
 
 end
@@ -30,24 +25,19 @@ end
 class BallPlayingDefender < Player
 
     def description
-        "The main job of the Ball Playing Defender is to stop the opposing attackers from playing and to
-        clear the ball from danger when required"
+        "The main job of the Ball Playing Defender is to stop the opposing attackers from playing and to clear the ball from danger when required"
     end
 
     def defend
-        "With a Defend duty, the Centre Defender will stay in the line with his defensive partner
-        and look to break up attaks mark opposing forwards and prevent 
-        the ball from getting into the box"
+        "With a Defend duty, the Ball Playing Defender will stay in the line with his defensive partner and look to break up attaks mark opposing forwards and prevent the ball from getting into the box."
     end
 
     def stoper
-        "With a Stoper Duty, the Ball Playing Defender will push ahead of the defensive line and close 
-        down players before they get to the area"
+        "With a Stoper Duty, the Ball Playing Defender will push ahead of the defensive line and close down players before they get to the area"
     end
 
     def cover
-        "With a Covering Duty, the Ball Playing Defender will drop a little deeper and sweep 
-        up through balls behind the defensive line"
+        "With a Covering Duty, the Ball Playing Defender will drop a little deeper and sweep up through balls behind the defensive line"
     end
 
 end
@@ -55,24 +45,40 @@ end
 class NoNonsenseCentreBack < Player
 
     def description
-        "The main job of the No-Nonsense Defender is to stop the opposing attackers from playing and to
-        clear the ball from danger when required"
+        "The main job of the No-Nonsense Defender is to stop the opposing attackers from playing and to clear the ball from danger when required."
     end
 
     def defend
-        "With a Defend duty, the Centre Defender will stay in the line with his defensive partner
-        and look to break up attaks mark opposing forwards and prevent 
-        the ball from getting into the box"
+        "With a Defend duty, the No-Nonsense Defender will stay in the line with his defensive partner and look to break up attaks mark opposing forwards and prevent the ball from getting into the box."
     end
 
     def stoper
-        "With a Stoper Duty, the No-Nonsense Defender will push ahead of the defensive line and close 
-        down players before they get to the area"
+        "With a Stoper Duty, the No-Nonsense Defender will push ahead of the defensive line and close down players before they get to the area"
     end
 
     def cover
-        "With a Covering Duty, the No-Nonsense Defender will drop a little deeper and sweep 
-        up through balls behind the defensive line"
+        "With a Covering Duty, the No-Nonsense Defender will drop a little deeper and sweep up through balls behind the defensive line"
     end
 
 end
+
+
+class Libero < Player
+
+    def description
+        "The Libero drops behind the defensive line, aiming to sweep up through balls, pick up extra attackers and make goal saving tackles, blocks and interceptions."
+    end
+
+    def defend
+        "With a Defend duty, the Libero will stay in the line with his defensive partner and look to break up attaks mark opposing forwards and prevent the ball from getting into the box."
+    end
+
+    def stoper
+        "With a Stoper Duty, the Libero will push ahead of the defensive line and close down players before they get to the area"
+    end
+
+    def cover
+        "With a Covering Duty, the Libero will drop a little deeper and sweep up through balls behind the defensive line"
+    end
+
+end    
--- lib/goal_keeper.rb
+++ lib/goal_keeper.rb
@@ -6,10 +6,7 @@ require_relative 'player'
 class Goalkeeper < Player
 
     def description
-        "This role is ideal for teams that have a more deeper defensive line. 
-        That way your goalkeeper will not be exposed too much if a ball falls behind
-        a high defensive line. In this role he will be used to save shots and clear crosses or 
-        long balls into the box."
+        "This role is ideal for teams that have a more deeper defensive line. That way your goalkeeper will not be exposed too much if a ball falls behind a high defensive line. In this role he will be used to save shots and clear crosses or long balls into the box."
     end
 
     def defend
@@ -22,24 +19,19 @@ end
 class SweeperKeeper < Player
 
     def description
-        "The sweeper keeper is the opposite of the Goalkeeper role. He will look to leave his
-        position more and will attempt to clear balls that fall between him and his defense.
-        When he is on the ball he will attempt more risky passes and will tend to dribble more."
+        "The sweeper keeper is the opposite of the Goalkeeper role. He will look to leave his position more and will attempt to clear balls that fall between him and his defense. When he is on the ball he will attempt more risky passes and will tend to dribble more."
     end
 
     def defend
-        "With a Defend Duty, the Sweeper Keeper will be a lot more cautious, but will still play
-        counterattaking balls when they are on"
+        "With a Defend Duty, the Sweeper Keeper will be a lot more cautious, but will still play counterattaking balls when they are on"
     end
     
     def suport
-        "With a suport Duty, the Sweeper Keeper will stray just outside of the penalty box and look to play
-        counter-attaking passes"
+        "With a suport Duty, the Sweeper Keeper will stray just outside of the penalty box and look to play counter-attaking passes"
     end
     
     def attack
-        "With a Attack Duty, the Sweeper Keeper will travel some distance outside his area
-        and be happy to move out with the ball at his feet"
+        "With a Attack Duty, the Sweeper Keeper will travel some distance outside his area and be happy to move out with the ball at his feet"
     end    
 
 
