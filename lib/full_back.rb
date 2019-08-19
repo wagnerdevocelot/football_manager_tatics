1b55686e23a35c205ce9addb52ca80f8653a7fc1
wagnerdevocelot <wagner.dev1337@gmail.com>
2019-08-19 19:44:18 -0300

    terminado o preset dos fullbacks

---
+++ lib/full_back.rb
@@ -0,0 +1,120 @@
+
+require_relative 'player'
+
+class FullBack
+
+    def description
+        "The Full back is a key playerin moden foottball, having to supplement his traditional defensive duties with overlapping runs down the wing to support forward play and help attacks overload the final third."
+    end
+
+    def defend
+        "With a defend duty, the Full Back will stay back with the defensive line and make simple possession passes down the flanks or into central midfield"
+    end
+
+    def support
+        "with a suport duty the Full Back will support the midfield by providing extra width and will look for crosses and through balls when the opportunity for each arises"
+    end
+
+    def attack
+        "With an attack Duty, the Full Back supplements his defensive responssibilities by overlaping the midfield and providing first crosses into the area."
+    end
+
+    def automatic
+        "With an automatic duty the Full Back will switch automatically between defend, support and attack duties, acording to the current mentality."
+    end
+end
+
+
+class WingBack
+
+    def description
+        "Usually playing out wide with no wing support, the wing back must fulfil all the attacking and defensive duties of wingers and full backs"
+    end
+
+    def defend
+        "With a defend duty, the Wing Back will stay back with the defensive line and make simple possession passes down the flanks or into central midfield"
+    end
+
+    def support
+        "with a suport duty the Wing Back will support the midfield by providing extra width and will look for crosses and through balls when the opportunity for each arises"
+    end
+
+    def attack
+        "With an attack Duty, the Wing Back supplements his defensive responssibilities by overlaping the midfield and providing first crosses into the area."
+    end
+
+    def automatic
+        "With an automatic duty the Wing Back will switch automatically between defend, support and attack duties, acording to the current mentality."
+    end
+end
+
+
+class NoNonsenseFullBack
+
+    def description
+        "the no nonsense fullback knows his strenghs and weaknesses and focus predominantly on his defensive duties, scarely going foward into a more attacking role."
+    end
+
+    def defend
+        "With a defend duty, the No-Nonsense Full Back will stay back with the defensive line and make simple possession passes down the flanks or into central midfield"
+    end
+
+    def support
+        "with a suport duty the No-Nonsense Full Back will support the midfield by providing extra width and will look for crosses and through balls when the opportunity for each arises"
+    end
+
+    def attack
+        "With an attack Duty, the No-Nonsense Full Back supplements his defensive responssibilities by overlaping the midfield and providing first crosses into the area."
+    end
+
+    def automatic
+        "With an automatic duty the No-Nonsense Full Back will switch automatically between defend, support and attack duties, acording to the current mentality."
+    end
+end
+
+
+class CompleteWingBack
+
+    def description
+        "The complete wing back loves to attack, and whilst he is capable of occupying his defensive duties, his natural inclinations is to affect the game in the oppositions defensive third by roaming from his position."
+    end
+
+    def defend
+        "With a defend duty, the Complete Wing Back will stay back with the defensive line and make simple possession passes down the flanks or into central midfield"
+    end
+
+    def support
+        "with a suport duty the Complete Wing Back will support the midfield by providing extra width and will look for crosses and through balls when the opportunity for each arises"
+    end
+
+    def attack
+        "With an attack Duty, the Complete Wing Back supplements his defensive responssibilities by overlaping the midfield and providing first crosses into the area."
+    end
+
+    def automatic
+        "With an automatic duty the Complete Wing Back will switch automatically between defend, support and attack duties, acording to the current mentality."
+    end
+end
+
+class InvertedWingBack
+
+    def description
+        "the inverted wing back will function defensively much like a standard full back or wing back. However, while a normal wing back will offer width to an attack, the inverted wing backwill atempt to drift inside and create apace for players around him."
+    end
+
+    def defend
+        "With a defend duty, the Inverted Wing Back will stay back with the defensive line and make simple possession passes down the flanks or into central midfield"
+    end
+
+    def support
+        "with a suport duty the Inverted Wing Back will support the midfield by providing extra width and will look for crosses and through balls when the opportunity for each arises"
+    end
+
+    def attack
+        "With an attack Duty, the Inverted Wing Back supplements his defensive responssibilities by overlaping the midfield and providing first crosses into the area."
+    end
+
+    def automatic
+        "With an automatic duty the Inverted Wing Back will switch automatically between defend, support and attack duties, acording to the current mentality."
+    end
+end
