cd3c22815f1b04faff6047dd2604dff70f593b8e
wagnerdevocelot <wagner.dev1337@gmail.com>
2019-08-21 19:54:48 -0300

    finalizado os atacantes

---
+++ lib/striker.rb
@@ -0,0 +1,157 @@
+require_relative 'player'
+
+
+class DeepLyingForward < Player
+
+    def description
+        "His primary function is link the attack to midfield The deep lying forward aims to drop deep into space and hold up the ball before supplying linking passes teammates, moving the ball back to midfield support, spreading it wide to the flanks and, if he has time to turn, playing in his strike partner"
+    end
+
+    def defend
+        "with a defend duty the Deep Lying Forward will stay a little lower and put pressure on defensive midfielders"
+    end
+
+    def support
+        "with a support duty the Deep Lying Forward will press the back line and put pressure on the central defenders"
+    end
+
+    def attack
+        "with a attack duty, the Deep Lying Forward will focus on scoring goals, rrely offers much help in constructing moves, preferring instead to remain central and sniff out opportunities in and around the box."
+    end
+end
+
+class AdvancedForward < Player
+
+    def description
+        "The advanced forward main role is to lead the line and looks to spearhead attaking moves, focal point of attacking moves and hes is required to both, score and create goals."
+    end
+
+    def defend
+        "with a defend duty the Advanced Forward will stay a little lower and put pressure on defensive midfielders"
+    end
+
+    def support
+        "with a support duty the Advanced Forward will press the back line and put pressure on the central defenders"
+    end
+
+    def attack
+        "with a attack duty, the Advanced Forward will focus on scoring goals, rrely offers much help in constructing moves, preferring instead to remain central and sniff out opportunities in and around the box."
+    end
+end
+
+class TargetMan < Player
+
+    def description
+        "A target man can transform an average team into a good one by using his sheer physicality to disrupt the oppositions defence and open up space for his strike partner and supporting midfielders"
+    end
+
+    def defend
+        "with a defend duty the Target Man will stay a little lower and put pressure on defensive midfielders"
+    end
+
+    def support
+        "with a support duty the Target Man will press the back line and put pressure on the central defenders"
+    end
+
+    def attack
+        "with a attack duty, the Target Man will focus on scoring goals, rrely offers much help in constructing moves, preferring instead to remain central and sniff out opportunities in and around the box."
+    end
+end
+
+
+class Poacher < Player
+
+    def description
+        "The poacher sits on the sholder of the least defender looking to break the defensive line and run onto through balls from midfield. Always ready to sun at his marker and is happy to croos the ball when required, his main aim is trying to puts the ball in the back of the net."
+    end
+
+    def defend
+        "with a defend duty the Poacher will stay a little lower and put pressure on defensive midfielders"
+    end
+
+    def support
+        "with a support duty the Poacher will press the back line and put pressure on the central defenders"
+    end
+
+    def attack
+        "with a attack duty, the Poacher will focus on scoring goals, rrely offers much help in constructing moves, preferring instead to remain central and sniff out opportunities in and around the box."
+    end
+end
+
+class CompleteForward < Player
+
+    def description
+        "A complete forward possesses all the technical attributes of a deep lying forward, the goal scoring ability of a poacher and the stengh and power of a target man"
+    end
+
+    def defend
+        "with a defend duty the Complete Forward will stay a little lower and put pressure on defensive midfielders"
+    end
+
+    def support
+        "with a support duty the Complete Forward will press the back line and put pressure on the central defenders"
+    end
+
+    def attack
+        "with a attack duty, the Complete Forward will focus on scoring goals, rrely offers much help in constructing moves, preferring instead to remain central and sniff out opportunities in and around the box."
+    end 
+end
+
+class PressingForward < Player
+
+    def description
+        "His main duty is to put pressure on the defensive line, chase down the man inpossession and any loose balls and generally prevent the opposing defence having any time on the ball, in an attacking sense he tends to prefer to bring other players into the game rather than create his own chances."
+    end
+
+    def defend
+        "with a defend duty the Pressing Forward will stay a little lower and put pressure on defensive midfielders"
+    end
+
+    def support
+        "with a support duty the Pressing Forward will press the back line and put pressure on the central defenders"
+    end
+
+    def attack
+        "with a attack duty, the Pressing Forward will focus on scoring goals, rrely offers much help in constructing moves, preferring instead to remain central and sniff out opportunities in and around the box."
+    end
+end
+
+
+class Trequartista < Player
+
+    def description
+        "While similar to the advanced playmaker in that he aims to drop into the holes between the oppositions midfield and defence, the trequartista does far less defensively and simply drifits around looking for space when the team is not in possession."
+    end
+
+    def defend
+        "with a defend duty the Trequartista will stay a little lower and put pressure on defensive midfielders"
+    end
+
+    def support
+        "with a support duty the Trequartista will press the back line and put pressure on the central defenders"
+    end
+
+    def attack
+        "with a attack duty, the Trequartista will focus on scoring goals, rrely offers much help in constructing moves, preferring instead to remain central and sniff out opportunities in and around the box."
+    end
+end
+
+
+class FalseNine < Player
+
+    def description
+        "The salse nine in some ways, similar to a more advanced attacking midfielder/playmaker role, is an unconventional striker or centre forward who drops deep into midfield. The pupose of this is that it creates a problem for opposing central defenders who can either follow him - leaving space behind them for onrushing midfielders, forwards or wingers to exloit or leave him to ave time and space to dribble or pick out a pass."
+    end
+
+    def defend
+        "with a defend duty the False Nine will stay a little lower and put pressure on defensive midfielders"
+    end
+
+    def support
+        "with a support duty the False Nine will press the back line and put pressure on the central defenders"
+    end
+
+    def attack
+        "with a attack duty, the False Nine will focus on scoring goals, rrely offers much help in constructing moves, preferring instead to remain central and sniff out opportunities in and around the box."
+    end
+end
